19c52e0be88a8d40e55139510425ff90192a3d4e
BiswasAmitav <biswasamit2648@gmail.com>
2022-08-11 23:09:12 +0530

    Rounded is done through this code

---
+++ Primitive Data type/src/primitive/dataTyppe/BDecimal.java
@@ -0,0 +1,17 @@
+package primitive.dataTyppe;
+import java.math.BigDecimal;
+public class BDecimal {
+    public static void main(String[] args) {
+
+        double x=1.05;
+        double y=2.55;
+
+        System.out.println(x/3);//it doesn't shows correct output....
+
+        //Its called rounding method..on java ........
+        BigDecimal d1=new BigDecimal("1.05");
+        BigDecimal d2=new BigDecimal("2.55");
+
+        System.out.println(d1.add(d2));//now it perform good ...
+    }
+}
