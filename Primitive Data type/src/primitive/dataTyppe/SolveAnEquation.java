75b90a4aba9bed88b9a7babde301bd2456473580
BiswasAmitav <biswasamit2648@gmail.com>
2022-08-12 21:06:34 +0530

    operator operation has done with precedence

---
+++ Primitive Data type/src/primitive/dataTyppe/SolveAnEquation.java
@@ -0,0 +1,11 @@
+package primitive.dataTyppe;
+
+public class SolveAnEquation {
+    public static void main(String[] args) {
+        int a =25;
+        float b =42.2323f;
+
+        double value = a*a + 2*(a*b) + b*b;
+        System.out.println("Value of the equaltion is : "+ value);
+    }
+}
--- Primitive Data type/src/primitive/dataTyppe/TypeCasting.java
+++ Primitive Data type/src/primitive/dataTyppe/TypeCasting.java
@@ -2,6 +2,11 @@ package primitive.dataTyppe;
 
 public class TypeCasting {
     public static void main(String[] args) {
+        int y=10;
+        short x=(short)y;
+
+
+        System.out.println(y);
 
     }
 }
