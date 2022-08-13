dbefec5e70884677e0ed6b930ca1ab0f88548b98
BiswasAmitav <biswasamit2648@gmail.com>
2022-08-13 11:44:26 +0530

    to add some problem statemnet

--- Primitive Data type/src/decision/makig/with/java/MakingDecisionWithIfElse.java
+++ Primitive Data type/src/decision/makig/with/java/MakingDecisionWithIfElse.java
@@ -1,4 +1,7 @@
 package decision.makig.with.java;
 
 public class MakingDecisionWithIfElse {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ Primitive Data type/src/flow/control/java/in/SwitchCase.java
@@ -0,0 +1,17 @@
+package flow.control.java.in;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+        //System.out.println("hello");
+        //instead of if-else we cn use switch ....
+
+//        switch (){
+//            case
+//        }
+
+        char ch='a';
+        switch (ch){
+
+        }
+    }
+}
